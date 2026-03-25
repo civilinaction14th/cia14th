72e99081c33a5c1094ea4bc3fb80c83a53533ed8
AdityaZulkarnaen <adityazulkarnaen083@gmail.com>
2026-03-25 18:08:22 +0700

    fix: events texture

--- src/modules/events/layout/DefaultEventLayout.tsx
+++ src/modules/events/layout/DefaultEventLayout.tsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const DefaultEventLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="relative min-h-screen background-auth w-full flex flex-col items-center overflow-hidden py-10 isolate">
+    <main className="relative min-h-screen bg-[linear-gradient(to_right,#0c1f31_47%,#0d334e_100%)] w-full flex flex-col items-center overflow-hidden py-10 isolate">
       {/* Bunga 1 (Kiri Bawah) */}
-      <div className="absolute bottom-0 left-0 w-50 h-50 md:w-62.5 md:h-62.5 lg:w-75 lg:h-75 z-5 pointer-events-none">
+      <div className="absolute -bottom-25 -left-20 w-50 h-50 md:w-62.5 md:h-62.5 lg:w-75 lg:h-75 z-5 pointer-events-none animate-[spin_15s_linear_infinite]">
         <Image
-          src="/auth/Bunga1.webp"
+          src="/images/Star.svg"
           alt="Bunga1"
           fill
           className="object-contain object-bottom-left"
@@ -15,9 +15,9 @@ const DefaultEventLayout = ({ children }: { children: React.ReactNode }) => {
         />
       </div>
 
-      <div className="absolute top-0 right-0 w-50 h-50 md:w-62.5 md:h-62.5 lg:w-75 lg:h-75 z-5 pointer-events-none">
+      <div className="absolute -top-5 -right-20 w-50 h-50 md:w-62.5 md:h-62.5 lg:w-75 lg:h-75 z-5 pointer-events-none animate-[spin_15s_linear_infinite]">
         <Image
-          src="/auth/Bunga2.webp"
+          src="/images/Star.svg"
           alt="Bunga2"
           fill
           className="object-contain object-top-right"
@@ -27,12 +27,12 @@ const DefaultEventLayout = ({ children }: { children: React.ReactNode }) => {
 
       {/* Texture overlay */}
       <div
-        className="absolute inset-0 bg-cover bg-start z-6 pointer-events-none"
+        className="absolute inset-0 bg-cover bg-start z-10 pointer-events-none"
         style={{
           backgroundImage: "url('/images/TextureBg.png')",
-          opacity: 0.48,
+          opacity: 0.58,
           mixBlendMode: "soft-light",
-          height: "110%",
+          height: "150%",
         }}
       />
       <div className="relative z-10 w-full px-4 md:px-8">{children}</div>
